b24dd3f806787dd2a09437a0825f5f23ca93d9f3
Gustavo-Gon <gon.gustavo@outlook.com>
2024-04-07 23:04:35 -0700

    Finished arrayListType.h

--- Array-Based-Lists/arrayListType.h
+++ Array-Based-Lists/arrayListType.h
@@ -106,27 +106,27 @@ protected:
     int maxSize;    //to store the maximum size of the list
 };
 
-template <class elemType>
+template <class elemType> //O(1)
 bool arrayListType<elemType>::isEmpty() const {
     return (length == 0);
 }
 
-template <class elemType>
+template <class elemType> //O(1)
 bool arrayListType<elemType>::isFull() const {
     return (length == maxSize);
 }
 
-template <class elemType>
+template <class elemType> //O(1)
 int arrayListType<elemType>::listSize() const {
     return length;
 }
 
-template <class elemType>
+template <class elemType> //O(1)
 int arrayListType<elemType>::maxListSize() const {
     return maxSize;
 }
 
-template <class elemType>
+template <class elemType> //O(n)
 void arrayListType<elemType>::print() const {
     for (int i = 0; i < length; i++)
         cout << list[i] << " ";
@@ -134,12 +134,12 @@ void arrayListType<elemType>::print() const {
     cout << endl;
 }
 
-template <class elemType>
+template <class elemType> //O(1)
 bool arrayListType<elemType>::isItemAtEqual(int location, const elemType &item) const {
     return(list[location] == item);
 }
 
-template <class elemType>
+template <class elemType> //O(n)
 void arrayListType<elemType>::insertAt(int location, const elemType &insertItem) {
     if (location < 0 || location >= maxSize)
         cerr << "The position of the item to be inserted "
@@ -155,7 +155,7 @@ void arrayListType<elemType>::insertAt(int location, const elemType &insertItem)
         }
 }  //end insertAt
 
-template <class elemType>
+template <class elemType> //O(1)
 void arrayListType<elemType>::insertEnd(const elemType &insertItem) {
     if (length >= maxSize)  //the list is full
         cerr << "Cannot insert in a full list" << endl;
@@ -165,7 +165,7 @@ void arrayListType<elemType>::insertEnd(const elemType &insertItem) {
     }
 } //end insertEnd
 
-template <class elemType>
+template <class elemType> //O(n)
 void arrayListType<elemType>::removeAt(int location) {
     if (location < 0 || location >= length)
         cerr << "The location of the item to be removed "
@@ -177,11 +177,125 @@ void arrayListType<elemType>::removeAt(int location) {
     }
 } //end removeAt
 
+template <class elemType> //O(1)
+void arrayListType<elemType>::retrieveAt(int location, elemType &retItem) const {
+    if (location < 0 || location >= length)
+        cerr << "The location of the item to be retrieved is "
+             << "out of range." << endl;
+    else
+        retItem = list[location];
+} //end retrieveAt
 
+template <class elemType> //O(n)
+void arrayListType<elemType>::replaceAt(int location, const elemType &repItem) {
+    if (location < 0 || location >= length)
+        cerr << "The location of the item to be replaced is "
+             << "out of range." << endl;
+    else
+        list[location] = repItem;
+} //end replaceAt
 
+template <class elemType> //O(1)
+void arrayListType<elemType>::clearList() {
+    length = 0;
+} //end clearList
 
+template <class elemType> //O(1)
+arrayListType<elemType>::arrayListType(int size) {
+    if (size < 0) {
+        cerr << "The array size must be positive. Creating "
+             << "an array of size 100. " << endl;
+        maxSize = 100;
+    }
+    else
+        maxSize = size;
 
+    length = 0;
+    list = new elemType[maxSize];
+    assert(list != NULL);
+}
 
+template <class elemType> //O(1)
+arrayListType<elemType>::~arrayListType() {
+    delete [] list;
+}
+
+template <class elemType> //O(n)
+arrayListType<elemType>::arrayListType(const arrayListType<elemType>& otherList) {
+    maxSize = otherList.maxSize;
+    length = otherList.length;
+    list = new elemType[maxSize];   //create the array
+    assert(list != NULL);           //terminate if unable to allocate memory space
+
+    for (int j = 0; j < length; j++) //copy otherList
+        list [j] = otherList.list[j];
+} //end copy constructor
+
+template <class elemType> //O(n)
+const arrayListType<elemType>& arrayListType<elemType>::operator=(const arrayListType<elemType> &otherList) {
+    if  (this != &otherList) { //avoid self-assignment
+        delete [] list;
+        maxSize = otherList.maxSize;
+        length = otherList.length;
+
+        list = new elemType[maxSize]; //create the array
+        assert(list != NULL);         //if unable to allocate memory space, terminate the program
+
+        for (int i = 0; i < length; i++)
+            list[i] = otherList.list[i];
+    }
+    return *this;
+}
+
+template <class elemType> //O(n)
+int arrayListType<elemType>::seqSearch(const elemType &item) const {
+    int loc;
+    bool found = false;
+
+    for (loc = 0; loc < length; loc++)
+        if (list[loc] == item) {
+            found = true;
+            break;
+        }
+    if (found)
+        return loc;
+    else
+        return -1;
+} //end seqSearch
+
+template <class elemType> //O(n)
+void arrayListType<elemType>::insert(const elemType &insertItem) {
+    int loc;
+    if (length == 0) //list is empty
+        list[length++] = insertItem; //insert the item and increment the length
+
+    else if (length == maxSize)
+        cerr << "Cannot insert in a full list." << endl;
+    else {
+        loc = seqSearch(insertItem);
+        if (loc == -1) //the item to be inserted does not exist in the list
+            list[length++] = insertItem;
+        else
+            cerr << "the item to be inserted is already in "
+                 << "the list. No duplicates are allowed." << endl;
+    }
+} //end insert
+
+template <class elemType> //O(n)
+void arrayListType<elemType>::remove(const elemType &removeItem) {
+    int loc;
+
+    if (length == 0)
+        cerr << "Cannot delete from an empty list." << endl;
+    else {
+        loc = seqSearch(removeItem);
+
+        if (loc != -1)
+            removeAt(loc);
+        else
+            cout << "The item to be deleted is not in the list." << endl;
+    }
+} //end remove
 
 
 #endif //DATASTRUCTURES_ARRAYLISTTYPE_H
